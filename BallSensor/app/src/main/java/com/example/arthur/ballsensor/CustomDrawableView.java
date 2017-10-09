1b2b5c718d971bf5086d6170130f2551b480b1c1
ArthurWenger <awenger91@gmail.com>
2017-10-09 14:31:57 +0400

    Ajout du projet sur Git

---
+++ BallSensor/app/src/main/java/com/example/arthur/ballsensor/CustomDrawableView.java
@@ -0,0 +1,59 @@
+package com.example.arthur.ballsensor;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomDrawableView extends View
+{
+	//private Bitmap mWood;
+	//private Bitmap mBitmap;
+	private float xPos = 100;
+	private float yPos = 100;
+	private float width;
+	private float height;
+	private final int radius = 50;
+	private Paint paint= new Paint();
+
+	public CustomDrawableView(Context context)
+	{
+		super(context);
+		init();
+	}
+	public CustomDrawableView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		init();
+	}
+
+	private void init(){
+		paint.setColor(android.graphics.Color.RED);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeWidth(5);
+	}
+
+	protected void onDraw(Canvas canvas)
+	{
+		canvas.drawCircle( xPos, yPos, radius, paint );
+	}
+
+	public void moveBall( float x, float y){
+		float newX = xPos+x;
+		if(newX < width && newX>radius)
+			xPos = xPos+x;
+
+		float newY = yPos+y;
+		if(newY < height && newY>radius)
+			yPos = yPos + y;
+		invalidate();
+	}
+
+	@Override
+	protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld){
+		super.onSizeChanged(xNew, yNew, xOld, yOld);
+		width = xNew;
+		height = yNew;
+	}
+}
---
+++ BallSensor/app/src/main/java/com/example/arthur/ballsensor/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.arthur.ballsensor;
+
+import android.app.Service;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+	private ToggleButton mTOn;
+	private Button mBPlus;
+	private Button mBMoins;
+	private SensorManager manager;
+	private Sensor mAccelerometer;
+	private boolean accelSupported;
+	private MySensorListener sensorListener;
+	private float xAcc, yAcc;
+	CustomDrawableView ballView;
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_main );
+		manager = (SensorManager) getSystemService( Service.SENSOR_SERVICE );
+		mAccelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorListener = new MySensorListener();
+		initView();
+	}
+
+	private void initView() {
+		mTOn = (ToggleButton) findViewById( R.id.tOn );
+		mBPlus = (Button) findViewById( R.id.bPlus );
+		mBPlus.setOnClickListener( this );
+		mBMoins = (Button) findViewById( R.id.bMoins );
+		mBMoins.setOnClickListener( this );
+		ballView = (CustomDrawableView) findViewById(R.id.ballsView);
+	}
+
+	@Override
+	public void onClick( View v ) {
+		switch ( v.getId() ) {
+			case R.id.bPlus:
+				break;
+			case R.id.bMoins:
+				break;
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		accelSupported = manager.registerListener(sensorListener, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+	}
+	@Override
+	public void onPause() {
+		if (accelSupported)
+			manager.unregisterListener(sensorListener, mAccelerometer);
+		super.onPause();
+	}
+
+	private class MySensorListener implements SensorEventListener {
+		@Override
+		public void onSensorChanged( SensorEvent event ) {
+			switch(event.sensor.getType()){
+				case Sensor.TYPE_ACCELEROMETER:
+					xAcc = event.values[0];
+					yAcc = event.values[1];
+					ballView.moveBall(xAcc,yAcc);
+					break;
+				case Sensor.TYPE_GYROSCOPE:
+					//mTvRes.setText( "x="+ event.values[0]+"\ny="+event.values[1]+"\nz="+event.values[2]);
+					break;
+				case Sensor.TYPE_MAGNETIC_FIELD:
+					//mTvRes.setText( "x="+ event.values[0]+"\ny="+event.values[1]+"\nz="+event.values[2]);
+					break;
+				case Sensor.TYPE_PROXIMITY:
+					//mTvRes.setText("dist="+ event.values[0]);
+					break;
+				default:
+					break;
+			}
+		}
+
+		@Override
+		public void onAccuracyChanged( Sensor sensor, int accuracy ) {
+
+		}
+	}
+}
