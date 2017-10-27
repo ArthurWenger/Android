5631f49c800c3b58565df26d276589946f3b5299
ArthurWenger <awenger91@gmail.com>
2017-10-27 09:09:27 +0400

    Generation aléatoire du labyrinthe

--- BallSensor/app/src/main/java/com/example/arthur/ballsensor/Interfaces/MazeGenerator.java
+++ BallSensor/app/src/main/java/com/example/arthur/ballsensor/Interfaces/MazeGenerator.java
@@ -7,9 +7,9 @@ import com.example.arthur.ballsensor.Objects.LineSegment2D;
 import java.util.Set;
 
 public interface MazeGenerator {
-	public void generate( float mazeWidth, float mazeHeight, float cellWidth, float cellHeight, MazeGeneratorDelegate delegate );
-	public PointF getStartLocation();
-	public PointF getFinishLocation();
-	public Set<LineSegment2D> getWalls();
-	public Set<PointF> getRandomRoomLocations( int numLocations, boolean exclusive ); //exclusive means it's never been returned before, and not a start or finish location
+	void generate( float mazeWidth, float mazeHeight, float cellWidth, float cellHeight, MazeGeneratorDelegate delegate );
+	PointF getStartLocation();
+	PointF getFinishLocation();
+	Set<LineSegment2D> getWalls();
+	Set<PointF> getRandomRoomLocations( int numLocations, boolean exclusive ); //exclusive means it's never been returned before, and not a start or finish location
 }
