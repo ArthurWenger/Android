5c493e2e75f780922bf4b1bf2a34c99fdbbca5fc
ArthurWenger <awenger91@gmail.com>
2017-10-27 12:54:49 +0400

    Ajout d'une méthode pour notifier la demande de permissions

--- BallSensor/app/src/main/java/com/example/arthur/ballsensor/Objects/SingleShotLocationProvider.java
+++ BallSensor/app/src/main/java/com/example/arthur/ballsensor/Objects/SingleShotLocationProvider.java
@@ -9,11 +9,14 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 
 public class SingleShotLocationProvider {
 
 	public interface LocationCallback {
 		void onNewLocationAvailable( Double[] location );
+
+		void onRequestNeeded();
 	}
 
 	// calls back to calling thread, note this is for low grain: if you want higher precision, swap the
@@ -21,63 +24,52 @@ public class SingleShotLocationProvider {
 	// call usually takes <10ms
 	public static void requestSingleUpdate( final Context context, final LocationCallback callback ) {
 		final LocationManager locationManager = (LocationManager) context.getSystemService( Context.LOCATION_SERVICE );
+		assert locationManager != null;
 		boolean isNetworkEnabled = locationManager.isProviderEnabled( LocationManager.NETWORK_PROVIDER );
 		if ( isNetworkEnabled ) {
+			Log.d("Location", "network available detected");
 			Criteria criteria = new Criteria();
 			criteria.setAccuracy( Criteria.ACCURACY_COARSE );
 			if ( ActivityCompat.checkSelfPermission( context, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED
 					     && ActivityCompat.checkSelfPermission( context, Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-				// TODO: Consider calling
-				//    ActivityCompat#requestPermissions
-				// here to request the missing permissions, and then overriding
-				//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-				//                                          int[] grantResults)
-				// to handle the case where the user grants the permission. See the documentation
-				// for ActivityCompat#requestPermissions for more details.
+				callback.onRequestNeeded();
 				return;
 			}
-			locationManager.requestSingleUpdate( criteria, new LocationListener() {
-				@Override
-				public void onLocationChanged( Location location ) {
-					callback.onNewLocationAvailable( new Double[] {location.getLatitude(), location.getLongitude()} );
-				}
-
-				@Override
-				public void onStatusChanged( String provider, int status, Bundle extras ) {
-				}
-
-				@Override
-				public void onProviderEnabled( String provider ) {
-				}
-
-				@Override
-				public void onProviderDisabled( String provider ) {
-				}
-			}, null );
+			locationManager.requestSingleUpdate( criteria, new MyLocationListener( callback ), null );
 		} else {
+			Log.d("Location", "network not available detected");
 			boolean isGPSEnabled = locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER );
 			if ( isGPSEnabled ) {
+				Log.d("Location", "gps enabled detected");
 				Criteria criteria = new Criteria();
 				criteria.setAccuracy( Criteria.ACCURACY_FINE );
-				locationManager.requestSingleUpdate( criteria, new LocationListener() {
-					@Override
-					public void onLocationChanged( Location location ) {
-						callback.onNewLocationAvailable( new Double[] {location.getLatitude(), location.getLongitude()} );
-					}
-
-					@Override
-					public void onStatusChanged( String provider, int status, Bundle extras ) {
-					}
-
-					@Override
-					public void onProviderEnabled( String provider ) {
-					}
-
-					@Override
-					public void onProviderDisabled( String provider ) {
-					}
-				}, null );
+				locationManager.requestSingleUpdate( criteria, new MyLocationListener( callback ), null );
 			}
 		}
+	}
+
+	private static class MyLocationListener implements LocationListener {
+		LocationCallback callback;
+		public MyLocationListener(final LocationCallback callback){
+			this.callback = callback;
+		}
+		@Override
+		public void onLocationChanged( Location location ) {
+			Log.d("Location", "onLocationChanged triggered");
+			callback.onNewLocationAvailable( new Double[] {location.getLatitude(), location.getLongitude()} );
+		}
+
+		@Override
+		public void onStatusChanged( String provider, int status, Bundle extras ) {
+		}
+
+		@Override
+		public void onProviderEnabled( String provider ) {
+		}
+
+		@Override
+		public void onProviderDisabled( String provider ) {
+		}
+
 	}
 }
