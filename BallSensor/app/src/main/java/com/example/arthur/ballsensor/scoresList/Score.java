0e7850d25d2a94adcc2a1a3357be032d875599a3
ArthurWenger <awenger91@gmail.com>
2017-10-30 14:58:03 +0400

    Suppression des commentaires inutiles

--- BallSensor/app/src/main/java/com/example/arthur/ballsensor/scoresList/Score.java
+++ BallSensor/app/src/main/java/com/example/arthur/ballsensor/scoresList/Score.java
@@ -5,9 +5,7 @@ import java.io.Serializable;
 public class Score implements Serializable {
 
 	private int rank;
-	//private String name;
 	private int value;
-	// a implementer
 	private double lat;
 	private double lng;
 
@@ -17,18 +15,7 @@ public class Score implements Serializable {
 		this.value = value;
 		this.lat = lat;
 		this.lng = lng;
-		/*
-		if( rank >4 || rank <0)
-			this.rank = 4;
-		else
-			this.rank = rank; */
 	}
-
-	/* public Score( int value) {
-		//this.name = name;
-		this.value = value;
-		this.rank = 4;
-	} */
 
 	public int getRank() {
 		return rank;
@@ -48,12 +35,4 @@ public class Score implements Serializable {
 	public double getLatitude() {
 		return lat;
 	}
-
-	/*public String getName() {
-		return name;
-	}*/
-
-	/*public void setName( String name ) {
-		this.name = name;
-	}*/
 }
