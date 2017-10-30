171f6ba829aaf68d34319242eb83793cfb5a2234
ArthurWenger <awenger91@gmail.com>
2017-10-30 15:24:26 +0400

    Ajout de commentaires

--- BallSensor/app/src/main/java/com/example/arthur/ballsensor/location/LocationListener.java
+++ BallSensor/app/src/main/java/com/example/arthur/ballsensor/location/LocationListener.java
@@ -1,7 +1,7 @@
 package com.example.arthur.ballsensor.location;
 
 /** Interface permettant d'écouter la classe "SingleShotLocationListener" pour récupérer la position
- *  de l'appareil ou notifier une demande de permission nécessaire **/
+ *  de l'appareil ou savoir si une demande de permission est recquise **/
 public interface LocationListener {
 	void onNewLocationAvailable( Double[] location );
 
